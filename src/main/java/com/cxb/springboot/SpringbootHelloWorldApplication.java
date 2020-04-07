b0f7549b06485c5a5dfe5e14effebedc56e29a04
ChenXb <810464826@qq.com>
2020-04-07 10:01:38 +0800

    Update SpringbootHelloWorldApplication.java

--- src/main/java/com/cxb/springboot/SpringbootHelloWorldApplication.java
+++ src/main/java/com/cxb/springboot/SpringbootHelloWorldApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootMybatisMycatApplication {
+public class SpringbootHelloWorldApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootMybatisMycatApplication.class, args);
